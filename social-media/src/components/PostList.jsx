59d6533a73468d869f0e3a6d42aaa4d0e3b67d4c
CoderSid063 <sudhansupanda063@gmail.in>
2023-11-21 14:32:52 +0530

    useEffect Hook

--- social-media/src/components/PostList.jsx
+++ social-media/src/components/PostList.jsx
@@ -1,23 +1,22 @@
 import Post from "./Post";
 import { PostList as PostListData } from "../store/post-list-store";
-import { useContext } from "react";
+import { useContext, useEffect } from "react";
 import WelcomeMessege from "./WelcomeMessege";
+
 const PostList = () => {
   const { postList, addInitialPosts } = useContext(PostListData);
 
-  const handleGetPostsClick = () => {
+  useEffect(() => {
     fetch("https://dummyjson.com/posts")
       .then((res) => res.json())
       .then((data) => {
         addInitialPosts(data.posts);
       });
-  };
+  }, []);
 
   return (
     <>
-      {postList.length === 0 && (
-        <WelcomeMessege onGetPostsClick={handleGetPostsClick} />
-      )}
+      {postList.length === 0 && <WelcomeMessege />}
       {postList.map((post) => (
         <Post key={post.id} post={post} />
       ))}
--- social-media/src/components/WelcomeMessege.jsx
+++ social-media/src/components/WelcomeMessege.jsx
@@ -1,8 +1,7 @@
-const WelcomeMessege = ({onGetPostsClick}) => {
+const WelcomeMessege = () => {
   return (
     <center className="welcome-messege">
       <h1 >There are no Posts</h1>
-      <button type="button" className="btn btn-primary" onClick={onGetPostsClick}>Get Posts From Server</button>
     </center>
   );
 };
